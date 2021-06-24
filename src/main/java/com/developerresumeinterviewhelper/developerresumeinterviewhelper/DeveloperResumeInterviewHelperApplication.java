242fabe20eb6d9e88be0fc5d154301742877aa97
ypd01018 <ypd01018@naver.com>
2021-06-24 18:35:55 +0900

    Initial commit

---
+++ src/main/java/com/developerresumeinterviewhelper/developerresumeinterviewhelper/DeveloperResumeInterviewHelperApplication.java
@@ -0,0 +1,13 @@
+package com.developerresumeinterviewhelper.developerresumeinterviewhelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeveloperResumeInterviewHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeveloperResumeInterviewHelperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/developerresumeinterviewhelper/developerresumeinterviewhelper/DeveloperResumeInterviewHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.developerresumeinterviewhelper.developerresumeinterviewhelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeveloperResumeInterviewHelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
